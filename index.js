4ecae55af78cc8c2c0982aa6f7f17748895fe080
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-11 01:47:55 -0400

    refactor: Moves modules to lib dirs.

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    commonjs: true,
-    "shared-node-browser": true,
     es6: true,
-    amd: true,
-    jest: true,
   },
   parserOptions: {
     ecmaVersion: "2018",
@@ -17,25 +13,8 @@ module.exports = {
       impliedStrict: true,
     },
   },
-  plugins: [
-    "react",
-  ],
-  settings: {
-    linkComponents: [
-      {name: "Link", linkAttribute: "to"}  
-    ]
-  },
   extends: [
-    // base
-    "./rules/base/best-practices",
-    "./rules/base/errors",
-    "./rules/base/es6",
-    "./rules/base/nodejs-commonjs",
-    "./rules/base/strict-mode",
-    "./rules/base/stylistic",
-    "./rules/base/variables",
-    // react
-    "./rules/react/general",
-    "./rules/react/jsx",
+    "./rules/react",
+    "./rules/base",
   ].map(require.resolve)
 }
