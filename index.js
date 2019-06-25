0fbe12e202cc16262e5eb508c16261b5428163cb
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-25 17:54:54 -0400

    docs: Updates JSDoc.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * ESLint config meta-package. This includes rules for base, import, and react.
+ *
+ * @return {object}
+ * The eslint config object.
+ */
 module.exports = {
   extends: [
     "@alexseitsinger/eslint-config-base",
