9a50025b6b412a00443b463ab090b8d19866cae6
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-25 17:12:28 -0400

    refactor: Changes to meta-package.

--- index.js
+++ index.js
@@ -1,37 +1,6 @@
-/**
- * @description The ESLint config object for base, react, and imports.
- * @return {Object} The ESLint config object to extend from.
- */
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-    jest: true,
-    commonjs: true,
-  },
-  globals: {
-    // Add some globals for Jest.
-    describe: "readonly",
-    it: "readonly",
-    test: "readonly",
-    expect: "readonly",
-    shallow: "readonly",
-    mount: "readonly",
-    render: "readonly",
-  },
-  parserOptions: {
-    ecmaVersion: "2018",
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-      globalReturn: false,
-      impliedStrict: true,
-    },
-  },
   extends: [
-    "./rules/react",
-    "./rules/base",
-    "./rules/import",
-  ].map(require.resolve)
+    "@alexseitsinger/eslint-config-base",
+    "@alexseitsinger/eslint-config-react",
+  ]
 }
