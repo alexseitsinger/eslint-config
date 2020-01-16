433e296c859724e102dc06d9e166df5e7234ae9b
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-01-16 17:36:48 -0500

    refactor: Adds group for rhl.

--- simple-import-sort/index.js
+++ simple-import-sort/index.js
@@ -18,6 +18,8 @@ module.exports = {
             "^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)"
           ],
 
+          ["^react-hot-loader(/boot)?$"],
+
           // Packages. `react` related packages come first.
           ["^react", "^prop-types", "^@?\\w"],
 
@@ -36,4 +38,4 @@ module.exports = {
       }
     ]
   }
-}
+};
