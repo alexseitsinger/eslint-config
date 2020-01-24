5c78b379e4cd052587571c9fa4f57cb0bef478c4
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-01-24 16:47:13 -0500

    feat: Adds config for markdown.

---
+++ markdown/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parserOptions: {
+    ecmaFeatures: {
+      impliedStrict: true
+    }
+  },
+  rules: {
+    strict: "off",
+    "no-undef": "off",
+    "no-unused-vars": "off",
+    "no-console": "off",
+    semi: "off",
+    "no-shadow": "off"
+  }
+};
