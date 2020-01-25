659d6b7e06706ab52e02eb864c4c086521905277
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-01-24 19:26:38 -0500

    feat: Adds extra exceptions for markdown.

--- markdown/index.js
+++ markdown/index.js
@@ -5,11 +5,20 @@ module.exports = {
     }
   },
   rules: {
+    // eslint rules
     strict: "off",
     "no-undef": "off",
     "no-unused-vars": "off",
     "no-console": "off",
     semi: "off",
-    "no-shadow": "off"
+    "no-shadow": "off",
+
+    // react rules
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": "off",
+    "react/jsx-no-undef": "off",
+
+    // import rules
+    "import/no-unresolved": "off"
   }
 };
