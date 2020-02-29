8cecd014069fd6d5badb3f56ad7048c8c74a57f5
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-02-29 01:04:16 -0500

    chore: Changes eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,65 @@
-const configs = require(".")
+const eslintRules = require("./rules/eslint")
+const eslintCommentsRules = require("./rules/eslint-comments")
+const nodeRules = require("./rules/node")
+const importRules = require("./rules/import")
+const prettierRules = require("./rules/prettier")
+const packageRules = require("./rules/package-json")
+const markdownRules = require("./rules/markdown")
+
+const parserOptions = {
+  ecmaVersion: 2020,
+  sourceType: "module",
+}
+
+const plugins = ["eslint-comments", "node", "import", "prettier"]
+const extended = ["prettier", "prettier/babel"]
+
+const enabled = {
+  ...eslintRules.enabled,
+  ...eslintCommentsRules.enabled,
+  ...nodeRules.enabled,
+  ...importRules.enabled,
+  ...prettierRules.enabled,
+}
+
+const disabled = {
+  ...eslintRules.disabled,
+  ...eslintCommentsRules.disabled,
+  ...nodeRules.disabled,
+  ...importRules.disabled,
+  ...prettierRules.disabled,
+}
 
 module.exports = {
   overrides: [
     {
       files: ["*.js"],
-      ...configs.javascript,
+      parserOptions,
+      plugins,
+      rules: {
+        ...enabled,
+        ...disabled,
+      },
+      extends: [...extended],
     },
     {
       files: ["*.md"],
-      ...configs.markdownJavascript,
+      plugins: [...plugins, "markdown"],
+      rules: {
+        ...enabled,
+        ...markdownRules.enabled,
+        ...disabled,
+        ...markdownRules.disabled,
+      },
+      extends: [...extended],
     },
     {
       files: ["package.json"],
-      ...configs.packageJson,
+      plugins: ["package-json"],
+      rules: {
+        ...packageRules.enabled,
+        ...packageRules.disabled,
+      },
     },
   ],
 }
