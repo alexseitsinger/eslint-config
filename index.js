062fc6e35a9a587f329e243d9f3d504313c5d208
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-02-28 23:47:23 -0500

    refactor: Adds factory configs to entry point.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-module.exports = {}
+const factory = require("./factory")
+
+const javascript = ["common", "javascript"]
+const javascriptReact = [...javascript, "react"]
+const javascriptReactRedux = [...javascriptReact, "redux"]
+const typescript = ["common", "typescript"]
+const typescriptReact = [...typescript, "react"]
+const typescriptReactRedux = [...typescriptReact, "redux"]
+const markdownTypescript = [...typescript, "markdown"]
+const markdownTypescriptReact = [...typescriptReact, "markdown"]
+const markdownTypescriptReactRedux = [...typescriptReactRedux, "markdown"]
+const markdownJavascript = [...javascript, "markdown"]
+const markdownJavascriptReact = [...javascriptReact, "markdown"]
+const markdownJavascriptReactRedux = [...javascriptReactRedux, "markdown"]
+const packageJson = ["package-json"]
+
+module.exports = {
+  javascript: factory(javascript),
+  javascriptReact: factory(javascriptReact),
+  javascriptReactRedux: factory(javascriptReactRedux),
+  typescript: factory(typescript),
+  typescriptReact: factory(typescriptReact),
+  typescriptReactRedux: factory(typescriptReactRedux),
+  markdownTypescript: factory(markdownTypescript),
+  markdownTypescriptReact: factory(markdownTypescriptReact),
+  markdownTypescriptReactRedux: factory(markdownTypescriptReactRedux),
+  markdownJavascript: factory(markdownJavascript),
+  markdownJavascriptReact: factory(markdownJavascriptReact),
+  markdownJavascriptReactRedux: factory(markdownJavascriptReactRedux),
+  packageJson: factory(packageJson),
+}
