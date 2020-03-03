5f2ba6e1ce9bc621e44a922316a0ec21b80307ca
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-03 13:01:59 -0500

    refactor: Updates local eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,68 +1,18 @@
-const eslintRules = require("./rules/eslint")
-const eslintCommentsRules = require("./rules/eslint-comments")
-const nodeRules = require("./rules/node")
-const importRules = require("./rules/import")
-const packageRules = require("./rules/package-json")
-const markdownRules = require("./rules/markdown")
-
-const parserOptions = {
-  ecmaVersion: 2020,
-  sourceType: "module",
-}
-const plugins = ["eslint-comments", "node", "import"]
-const rules = {
-  enabled: {
-    // Enabled rules...
-    ...eslintRules.enabled,
-    ...eslintCommentsRules.enabled,
-    ...nodeRules.enabled,
-    ...importRules.enabled,
-  },
-  disabled: {
-    // Disabled rules...
-    ...eslintRules.disabled,
-    ...eslintCommentsRules.disabled,
-    ...nodeRules.disabled,
-    ...importRules.disabled,
-  },
-}
-const env = {
-  node: true,
-}
+const configs = require(".")
 
 module.exports = {
   overrides: [
     {
       files: ["*.js"],
-      env,
-      parserOptions,
-      plugins,
-      rules: {
-        ...rules.enabled,
-        ...rules.disabled,
-      },
+      ...configs.javascript,
     },
     {
       files: ["*.md"],
-      env,
-      parserOptions,
-      plugins: [...plugins, "markdown"],
-      rules: {
-        ...rules.enabled,
-        ...markdownRules.enabled,
-        ...rules.disabled,
-        ...markdownRules.disabled,
-      },
+      ...configs.markdownJavascript,
     },
     {
       files: ["package.json"],
-      parserOptions,
-      env,
-      plugins: ["package-json"],
-      rules: {
-        ...packageRules.enabled,
-        ...packageRules.disabled,
-      },
+      ...configs.package,
     },
   ],
 }
