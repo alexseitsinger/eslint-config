2e80e12fab85d429dc349289fe9cf67ff8451cb1
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-04 18:10:08 -0500

    refactor: Adds import resolvers.

--- plugins/import/options.js
+++ plugins/import/options.js
@@ -3,5 +3,13 @@ module.exports = {
     "import/core-modules": ["fs", "path", "child_process"],
     "import/ignore": ["node_modules", ".yalc"],
     "import/external-module-folders": ["node_modules", ".yalc"],
-  },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
+      },
+      jest: {
+        jestConfigFile: "./jest.config.js"
+      }
+    }
+  }
 }
--- plugins/jest/options.js
+++ plugins/jest/options.js
@@ -1,13 +1,6 @@
 module.exports = {
   env: {
-    "jest": true,
-    "jest/globals": true,
-  },
-  settings: {
-    "import/resolver": {
-      jest: {
-        jestConfigFile: "./jest.config.js",
-      },
-    },
-  },
+    jest: true,
+    "jest/globals": true
+  }
 }
