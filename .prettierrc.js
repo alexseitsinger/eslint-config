707e7c60f9f70c0e2f87a04abf984c13651d860e
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-04 18:08:06 -0500

    chore: Updates prettier config.

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,12 +2,12 @@ module.exports = {
   printWidth: 80,
   tabWidth: 2,
   singleQuote: false,
-  trailingComma: "es5",
+  trailingComma: "none",
   bracketSpacing: true,
   jsxBracketSameLine: true,
   parser: "babel",
   semi: false,
   useTabs: false,
   proseWrap: "preserve",
-  arrowParens: "avoid",
+  arrowParens: "avoid"
 }
