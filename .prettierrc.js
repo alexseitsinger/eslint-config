f42d7065f0a8071e3f9382b1b4663a99f04bf6a9
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-06 17:12:54 -0500

    chore: Adds trailing commas.

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,12 +2,12 @@ module.exports = {
   printWidth: 80,
   tabWidth: 2,
   singleQuote: false,
-  trailingComma: "none",
+  trailingComma: "es5",
   bracketSpacing: true,
   jsxBracketSameLine: true,
   parser: "babel",
   semi: false,
   useTabs: false,
   proseWrap: "preserve",
-  arrowParens: "avoid"
+  arrowParens: "avoid",
 }
