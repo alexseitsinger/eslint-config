7739919476b353b1e2576dd648cd044eaad5bc6a
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-06 20:10:26 -0500

    feat: Adds unicorn rules.

---
+++ plugins/unicorn/patches.js
@@ -0,0 +1,12 @@
+module.exports = {
+  eslint: {
+    "no-nested-ternary": "off",
+    "prefer-destructuring": [
+      "error",
+      {
+        object: true,
+        array: false,
+      },
+    ],
+  },
+}
